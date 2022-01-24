c8ae0fd95d75ab2fcf6412750b5b806b21551274
NazarShtereb <74925453+NazarShtereb@users.noreply.github.com>
2022-01-24 23:20:32 +0200

    Update Homework1.py

--- Homework1.py
+++ Homework1.py
@@ -1,26 +1,28 @@
-#Homework 1 "Python Basics"
+# Homework 1 "Python Basics"
 import random
 
-rlist = []                                              #create a blank list
-i = 0                                                   #create variable for the loop iteration
-for i in range(100):                                    #create for loop with 100 iterations
-    rlist.append(random.randrange(0,1000,1))            #add random numbers in range 0 - 1000
-n = len(rlist)                                          #create a variable with the list lenght value
-for n in range(len(rlist)-1, 0, -1):                    #going through every number of the list, starting from the end, with step -1
-   for i in range(n):
-        if rlist[i] > rlist[i+1]:                       #if first number is bigger than second number, then
-            rlist[i],rlist[i+1] = rlist[i+1],rlist[i]   #rearrange them
-print(f'A list of numbers: {rlist}')                                            #print the list
 
-even = []                                               #create a blank list for even numbers
-odd = []                                                #create a blank list for odd numbers
+rlist = []  # create a blank list
+i = 0  # create variable for the loop iteration
+for i in range(100):  # create for loop with 100 iterations
+    rlist.append(random.randrange(0, 1000, 1))  # add random numbers in range 0 - 1000
+n = len(rlist)  # create a variable with the list length value
+for n in range(len(rlist) - 1, 0, -1):  # going through every number of the list, starting from the end, with step -1
+    for i in range(n):
+        if rlist[i] > rlist[i + 1]:  # if first number is bigger than second number, then
+            rlist[i], rlist[i + 1] = rlist[i + 1], rlist[i]  # rearrange them
+print(f'A list of numbers: {rlist}')  # print the list
 
-for i in rlist:                                         #loop through an every element in the list of all elements
-    if i % 2: even.append(i)                            #check if the number is even. If it is even then add it to the even list
-    else: odd.append(i)                                 #if not, so it is odd, then add it to the odd list
-        
-print(f'An avg of even numbers: {sum(even)/len(even):.2f}')                              #calculating an average for the list of even numbers, then print it
-print(f'An avg of odd numbers: {sum(odd)/len(odd):.2f}')                                #calculating an average for the list of odd numbers, then print it
+even = []  # create a blank list for even numbers
+odd = []  # create a blank list for odd numbers
 
+for i in rlist:  # loop through an every element in the list of all elements
+    if i % 2:
+        even.append(i)  # check if the number is even. If it is even then add it to the even list
+    else:
+        odd.append(i)  # if not, so it is odd, then add it to the odd list
 
-
+print(
+    f'An avg of even numbers: {sum(even) / len(even):.2f}')  # calculating an average for the list of even numbers
+print(
+    f'An avg of odd numbers: {sum(odd) / len(odd):.2f}')  # calculating an average for the list of odd numbers
