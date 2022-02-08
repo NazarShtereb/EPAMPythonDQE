1e1fa4e1eff5468063a64b062ec3af7ea3313012
Nazar_Shtereb <shtereb@ukr.net>
2022-02-08 21:02:48 +0200

    added homework4-2

--- Homework4-1.py
+++ Homework4-1.py
@@ -2,21 +2,27 @@
 from random import randint, randrange
 from string import ascii_lowercase
 from collections import OrderedDict, Counter
+from logging import exception
 
 
 # create a blank list for dicts
 def create_list(dictCnt=3, kCnt=5, vStart=0, vEnd=10):
-    dictCnt = dictCnt  # create a variable for a number of random dicts
-    dictList = []
-    for i in range(dictCnt):
-        # create dictCnt dicts with letter keys
-        dictList.append(dict.fromkeys(ascii_lowercase[0:randint(2, kCnt)], 0))
-    for dictElement in dictList:
-        for key, value in dictElement.items():  # iterate through every item of dicts in the list
-            randValue = randint(vStart, vEnd)
-            dictElement.update({key: randValue})  # populate dicts with random values
-    print('Created a list of dicts:\n ', dictList)
-    return dictList
+    try:
+        dictCnt = dictCnt  # create a variable for a number of random dicts
+        dictList = []
+        for i in range(dictCnt):
+            # create dictCnt dicts with letter keys
+            dictList.append(dict.fromkeys(ascii_lowercase[0:kCnt], 0))
+        for dictElement in dictList:
+            for key, value in dictElement.items():  # iterate through every item of dicts in the list
+                randValue = randint(vStart, vEnd)
+                dictElement.update({key: randValue})  # populate dicts with random values
+        print('Created a list of dicts:\n ', dictList)
+        return dictList
+    except TypeError:
+        exception("Incorrect type of parameters")
+    except ValueError:
+        exception("Incorrect values")
 
 
 # get all keys from dicts in the list
@@ -45,18 +51,18 @@ def dict_concat(dict_list):
 
 # get concatenated dict with sorted values
 def get_sorted(dict_list):
-    # sorting the dict by values desc
-    result = OrderedDict(sorted(dict_concat(dict_list).items(), key=lambda x: x[1], reverse=True))
-    # get first keys with max value
-    finalDict = {}
     try:
+        # sorting the dict by values desc
+        result = OrderedDict(sorted(dict_concat(dict_list).items(), key=lambda x: x[1], reverse=True))
+        # get first keys with max value
+        finalDict = {}
         for i, v in result.items():
             if i[0] not in (x[0] for x in finalDict.keys()):
                 finalDict[i] = v
         return finalDict
-    except IndexError:
-        print('Index error')
+    except ValueError:
+        exception("Incorrect list input")
 
 
-list_dicts = create_list(randrange(2, 4, 1), 10, 1, 100)
-print('Final dict with max values:\n ', get_sorted(list_dicts))
+list_dicts = create_list(dictCnt=3, kCnt=randint(2, 5), vStart=5, vEnd=10)
+print('Final dict with max values:\n ', get_sorted(dict_list=list_dicts))
